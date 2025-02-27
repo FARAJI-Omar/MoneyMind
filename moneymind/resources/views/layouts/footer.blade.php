3fff3d57e5fb97227ac39d3f8b1da78777a7de78
F99 <farajiomar99@gmail.com>
2025-02-27 23:35:40 +0000

    Update views with footer

---
+++ moneymind/resources/views/layouts/footer.blade.php
@@ -0,0 +1,27 @@
+
+<footer style="background-color: black; color: white; padding: 40px 0; text-align: center; width: 100%; border-top: 4px solid lightgray;">
+        <h2 style="margin: 0 0 20px; font-size: 28px;">MoneyMind</h2>
+        
+        <div style="display: flex; justify-content: center; gap: 20px; margin-bottom: 20px; font-size: 15px; font-family: 'Poppins', sans-serif; font-weight: 600;">
+            <a href="#" style="color: white; text-decoration: none;">Privacy Policy</a>
+            <a href="#" style="color: white; text-decoration: none;">Security</a>
+            <a href="#" style="color: white; text-decoration: none;">Terms & Condition</a>
+            <a href="#" style="color: white; text-decoration: none;">Contact Us</a>
+        </div>
+        
+        <div style="display: flex; justify-content: center; gap: 600px; padding: 30px 0;">
+            <p style="margin: 20px 0; font-size: 13px;">Copyright © Easy Frontend, All rights reserved</p>
+
+            <div style="display: flex; justify-content: center; gap: 15px;">
+                <a href="#" style="display: inline-block; width: 30px; height: 30px; border: 1px solid white; border-radius: 50%; text-align: center; line-height: 30px; font-size: 16px; text-decoration: none; color: white;"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#" style="display: inline-block; width: 30px; height: 30px; border: 1px solid white; border-radius: 50%; text-align: center; line-height: 30px; font-size: 16px; text-decoration: none; color: white;"><i class="fa-brands fa-twitter"></i></a>
+                <a href="#" style="display: inline-block; width: 30px; height: 30px; border: 1px solid white; border-radius: 50%; text-align: center; line-height: 30px; font-size: 16px; text-decoration: none; color: white;"><i class="fa-brands fa-pinterest"></i></a>
+                <a href="#" style="display: inline-block; width: 30px; height: 30px; border: 1px solid white; border-radius: 50%; text-align: center; line-height: 30px; font-size: 16px; text-decoration: none; color: white;"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+        </div>
+    </footer>
+
+
+
+</body>
+</html>
