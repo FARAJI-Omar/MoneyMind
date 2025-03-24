259034f6394f1d558faaab8990300e272b725745
F99 <farajiomar99@gmail.com>
2025-03-24 23:08:04 +0000

    Change due_date column type from date to integer in recurring expenses migration

--- moneymind/database/migrations/2025_02_25_101332_create_recurring_expenses_table.php
+++ moneymind/database/migrations/2025_02_25_101332_create_recurring_expenses_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->decimal('price', 10, 2);
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained('expense_categories');
-            $table->date('due_date');
+            $table->integer('due_date');
             $table->timestamps();
         });
     }
